ac8fb623ea527ea6295834a7533f54f607193153
megan-castleton <megan.castleton@telicent.io>
2024-12-16 19:40:27 +0000

    update base

--- vitest.config.ts
+++ vitest.config.ts
@@ -5,6 +5,7 @@ import viteConfig from './vite.config'
 export default mergeConfig(
   viteConfig,
   defineConfig({
+    base: "/fire-door/",
     test: {
       environment: 'jsdom',
       exclude: [...configDefaults.exclude, 'e2e/**'],

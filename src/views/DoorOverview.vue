5a711c33ab26adb1a19ac8e1183c35da1c8738d4
megan-castleton <megan.castleton@telicent.io>
2024-12-17 18:29:45 +0000

    update scanner

--- src/views/DoorOverview.vue
+++ src/views/DoorOverview.vue
@@ -4,8 +4,11 @@ import {
 } from "vue";
 
 const previousSubmissions: Ref<any> = ref(null);
+const props = defineProps<{
+  formid: string,
+}>();
 
-async function getSubmission() {
+async function getSubmissionOnClick() {
     previousSubmissions.value = await fetch('https://kcmpmbkzfjbiagr.form.io/firesafetychecker/submission')
     .then(response => response.json())
     .then(actualData => {
@@ -14,17 +17,20 @@ async function getSubmission() {
     })
     console.log(previousSubmissions)
 }
-
+function showItem(item) {
+    console.log(item)
+}
 </script>
 
 <template>
     <div class="prev-submissions">
-        <h3>Previous Submissions</h3>
-        <v-btn @click="getSubmission()"> View Pre Selected Form </v-btn>
+        <v-btn @click="getSubmissionOnClick()"> View Pre Selected Form </v-btn>
         <div>
             <div v-if="previousSubmissions !== null">
                 <v-expansion-panels>
-                    <v-expansion-panel v-for="(x, i) in previousSubmissions" 
+                    <v-expansion-panel 
+                        v-for="(x, i) in previousSubmissions" 
+                        @click="showItem($event)"
                         v-bind:key="i"
                         class="pb-3" 
                         value="header">
--- src/views/ScanBarcode.vue
+++ src/views/ScanBarcode.vue
@@ -6,18 +6,63 @@ import {
 import { QrcodeStream } from 'vue3-qrcode-reader'
   // @ts-ignore
 const qrValue: Ref<string> = ref("Qr value - this will be a unique ID we will have to generate qith a sticker, scan a QR code it works currently")
+const previousSubmissions: Ref<any> = ref(null);
+const toggleQr: Ref<boolean> = ref(true);
+
 function onDecode (event: string) {
   console.log('hi', event)
   qrValue.value = event;
+  console.log(qrValue.value.split('id=')[1])
+  getSubmissionOnClick(qrValue.value.split('id=')[1])
 }
 
+async function getSubmissionOnClick(qrId:string) {
+    previousSubmissions.value = await fetch(`https://kcmpmbkzfjbiagr.form.io/${qrId}/submission`)
+    .then(response => response.json())
+    .then(actualData => {
+        console.log(actualData, 'boo')
+        toggleQr.value = false;
+        return actualData.reverse()
+    })
+}
+
+function scanNewQr() {
+    toggleQr.value = true;
+    previousSubmissions.value = null;
+}
+function showItem(item: any) {
+    console.log(item)
+}
 </script>
 
 <template>
-    <div class="qr-scanner">
+    <div v-if="toggleQr" class="qr-scanner">
         <qrcode-stream @decode="onDecode($event)"></qrcode-stream>
     </div>
     {{ qrValue }}
+    <v-btn v-if="!toggleQr" @click="scanNewQr()">Scan new QR</v-btn>
+    <div v-if="previousSubmissions !== null && !toggleQr">
+        <v-expansion-panels class="pt-5">
+            <v-expansion-panel 
+                v-for="(x, i) in previousSubmissions" 
+                @click="showItem($event)"
+                v-bind:key="i"
+                class="pb-3" 
+                value="header">
+                <v-expansion-panel-title hide-actions>
+                    Report {{ i + 1}}
+                </v-expansion-panel-title>
+                <v-expansion-panel-text>
+                    Inpection Date: {{ x.created }}<br>
+                    Is following checks meet safety criteria: <br>
+                    Glass inspection: {{ x.data.confirmAllAreSafe.glass }}<br>
+                    Hinges inspection: {{ x.data.confirmAllAreSafe.glass }}<br>
+                    Overall inspection: {{ x.data.confirmAllAreSafe.glass }}<br>
+                    Additional Notes: {{ x.data.notes }}
+                </v-expansion-panel-text>
+            </v-expansion-panel>
+                </v-expansion-panels>
+    </div>
 </template>
   
   <style>
